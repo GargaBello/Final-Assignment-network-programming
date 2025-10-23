aa7828b97688a142cae591434b082a8fcad1bf71
Sebastian_Saastamoinen <sebastian.jv.saastamoinen@outlook.com>
2025-10-23 14:29:33 +0200

    Some small amount of work

--- meteor/meteor/include/game.hpp
+++ meteor/meteor/include/game.hpp
@@ -8,9 +8,10 @@
 
 namespace meteor {
 	struct game {
-		int MAP_WIDTH = 900, MAP_HEIGHT = 900, MAP_X_POSITION = 0, MAP_Y_POSITION = 0;
+		int MAP_WIDTH = 900, MAP_HEIGHT = 900, MAP_X_POSITION = 20, MAP_Y_POSITION = 20, SPAWN_AREA_OFFSET = 8;
 
 		void set_map_size(Rectangle rec);
+		void create_terrain(int width, int height, int posx, int posy, int offset);
 
 		void init();
 		void update();
@@ -18,5 +19,7 @@ namespace meteor {
 
 
 		Rectangle m_background_map = { 0, 0, 0, 0 };
+
+		std::vector<std::vector<terrain>> all_terrain;
 	};
 }
